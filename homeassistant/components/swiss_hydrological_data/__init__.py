f316b132f5e5d6863d4ff86ec9c65a51b64f4bfd
Bouni <bouni@owee.de>
2025-01-17 10:44:11 +0100

    Fix setup issue

--- homeassistant/components/swiss_hydrological_data/__init__.py
+++ homeassistant/components/swiss_hydrological_data/__init__.py
@@ -6,17 +6,17 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    await hass.config_entries.async_forward_entry_setups(entry, Platform.SENSOR)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, Platform.SENSOR)
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
